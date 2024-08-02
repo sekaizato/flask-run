97c6000a6135f19fcee3abee2811601ba444a122
sekaizato <68102428+sekaizato@users.noreply.github.com>
2024-08-02 09:51:39 +0700

    perf: comment on db

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from app.modules import hello, users, test
-from app.database.postgres import db
+# from app.database.postgres import db
 
 from config import Config
 
@@ -12,7 +12,7 @@ from config import Config
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
+    # db.init_app(app)
     app.register_blueprint(url_prefix="/", blueprint=hello.blueprint)
     app.register_blueprint(url_prefix='/api/test', blueprint=test.blueprint)
     app.register_blueprint(url_prefix='/api/users', blueprint=users.blueprint)
